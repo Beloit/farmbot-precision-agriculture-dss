3bf0b1357cc5df51f20463f8264e5499538f77c6
Cameron Dunn <camdunn54321@gmail.com>
2014-04-21 17:43:58 -0700

    Created some real tests for the JobStatusAccessor

--- src/test/scala/aws/dynamo/JobStatusAccessorTest.scala
+++ src/test/scala/aws/dynamo/JobStatusAccessorTest.scala
@@ -2,13 +2,21 @@ package dynamo
 
 import dynamo.JobStatusAccessor
 import org.scalatest.FlatSpec
-import awscala.CredentialsLoader
+import awscala.{Region, CredentialsLoader}
 import helper.RequiresAWS
 import types.JobInfo
+import awscala.dynamodbv2.{Table, DynamoDB}
+import aws.UsesPrefix
+import constants.JobStatusTableConstants
 
-class JobStatusAccessorTest extends FlatSpec with RequiresAWS {
-  "creating two identical jobs" should "have unique IDs" in {
-    val ji : JobInfo = new JobInfo {
+class JobStatusAccessorTest extends FlatSpec with RequiresAWS with UsesPrefix {
+  implicit val dynamo = DynamoDB.at(Region.Oregon)
+  implicit val const = JobStatusTableConstants
+
+  var table: Table = dynamo.table(build(const.TABLE_NAME)).get
+
+  def withJob(testCode: String => Any) {
+    val ji = new JobInfo {
       attempt = 0
       channel = "TestChannel"
       channelVersion = 1
@@ -19,9 +27,23 @@ class JobStatusAccessorTest extends FlatSpec with RequiresAWS {
 
     val jsa = new JobStatusAccessor
 
-    val id1 = jsa.addEntry(ji)
-    val id2 = jsa.addEntry(ji)
+    val id = jsa.addEntry(ji)
 
-    assert(id1 != id2, "Job IDs are not unique")
+    try {
+      testCode(id)
+    }
+    finally dynamo.deleteItem(table, id)
   }
+
+  "creating two identical jobs" should "have unique IDs" in withJob { id1 =>
+    withJob { id2 =>
+      assert(id1 != id2, "Job IDs are not unique")
+    }
+  }
+
+  "creating a job" must "show up in dynamo" in withJob { id =>
+    assert(dynamo.get(table, id).isDefined)
+  }
+
+
 }
